14988f9c200b83b7586610655895b53d4efb6a02
Alexandr <durgen2014@gmail.com>
2023-04-03 23:54:44 +0300

    Задача на лайв-кодинг решение

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+/**
+Задача на лайв-кодинг
+        Напишите пример обработки нескольких исключений в одном блоке catch.
+**/
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            // Код, который может выбросить исключения
+            Scanner scanner = new Scanner(System.in);
+            int a = scanner.nextInt();
+            int b = 10 / a;
+            String str = null;
+            System.out.println(str.length());
+        } catch (ArithmeticException | NullPointerException e) {
+            System.out.println("Произошла ошибка: " + e.getMessage());
+        }
     }
 }
